364aa0116cf7d2010f75589d403de20cfd648804
Bailey Lievens <baileylievens@hotmail.be>
2021-05-23 16:26:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.wv_website);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.buur.baileylievens.be/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
